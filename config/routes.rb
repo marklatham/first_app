1024b0a4f52e68a2c9975b69c0e1c2bd6c243e22
Mark Latham <mark@votermedia.org>
2014-10-12 16:51:06 -0700

    Changes by: $ rails generate pages:users; & let it change root route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Infocoop::Application.routes.draw do
-  root 'home#index'
+  root to: 'visitors#index'
+  # root 'home#index'
   devise_for :users
+  resources :users
   get '/faq', :to => 'pages#faq', :as => :faq
 end
