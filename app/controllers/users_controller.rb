0ec8dbbf9ea2685e65390304244494194f4fd113
Mark Latham <mark@votermedia.org>
2014-10-16 14:41:03 -0700

    Admin nav link. Admin requirement moved to user_policy.rb file.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    unless @user == current_user
-      redirect_to :back, :alert => "Access denied."
-    end
+    authorize @user
   end
 
 end
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -10,4 +10,8 @@ class UserPolicy
     @current_user.admin?
   end
 
+  def show?
+    @current_user.admin? or @current_user == @user
+  end
+
 end
