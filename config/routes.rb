be63f04037a892740d246c6ca911c838dbcea7a3
Mark Latham <mark@votermedia.org>
2014-10-23 16:10:58 -0700

    Static content pages: about, faq, terms, privacy.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Infocoop::Application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   resources :users
-  get '/faq', :to => 'pages#faq', :as => :faq
+  get '/about',           :to => 'visitors#about',           :as => :about
+  get '/faq',             :to => 'visitors#faq',             :as => :faq
+  get '/privacy',         :to => 'visitors#privacy',         :as => :privacy
+  get '/terms',           :to => 'visitors#terms',           :as => :terms
 end
