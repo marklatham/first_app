2e1e77fb6318e28b968fa34b9c584b115c2da9d9
Mark Latham <mark@votermedia.org>
2014-10-26 13:09:39 -0700

    Customize devise routes, following github.com/plataformatec/devise/wiki/How-To:-Change-the-default-sign_in-and-sign_out-routes#another-simple-way-to-do-the-same-thing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Infocoop::Application.routes.draw do
   root to: 'visitors#index'
-  devise_for :users
+  devise_for :users, :path => '', :path_names => {:sign_up => 'register', :sign_in => 'login', :sign_out => 'logout'}
   resources :users
   get '/about',           :to => 'visitors#about',           :as => :about
   get '/faq',             :to => 'visitors#faq',             :as => :faq
