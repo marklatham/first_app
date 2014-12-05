c17c36236701a2486dc637ed072613d7d04d5401
Mark Latham <mark@votermedia.org>
2014-12-05 14:09:47 -0800

    Add title field to posts.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order(updated_at: :desc) }
   validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 100 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141205204210) do
+ActiveRecord::Schema.define(version: 20141205215847) do
 
   create_table "posts", force: true do |t|
     t.text     "content"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
   end
 
   add_index "posts", ["user_id", "created_at", "updated_at"], name: "index_posts_on_user_id_and_created_at_and_updated_at", using: :btree
