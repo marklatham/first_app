fc0733d3d53bb0e2b03f285821ad6a03ca2f4951
Mark Latham <mark@votermedia.org>
2014-12-07 12:48:33 -0800

    User can create posts and see list of own posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,29 @@
 class PostsController < ApplicationController
+  before_filter :authenticate_user!
+#  after_action :verify_authorized
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:notice] = "Post created!"
+      redirect_to root_url
+    else
+      flash[:alert] = "Sorry, couldn't create post. Try again?"
+      render 'posts/new'
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+  
 end
