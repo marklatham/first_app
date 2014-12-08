b544ed13b4ed623b8dbff1ed20f34c24e85f807c
Mark Latham <mark@votermedia.org>
2014-12-07 17:40:51 -0800

    Link & redirect to list of a user's posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
     @post = current_user.posts.build(post_params)
     if @post.save
       flash[:notice] = "Post created!"
-      redirect_to root_url
+      redirect_to current_user
     else
       flash[:alert] = "Sorry, couldn't create post. Try again?"
       render 'posts/new'
