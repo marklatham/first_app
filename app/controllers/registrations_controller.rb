815119ea713813a39a9bd4d52a196bbe55b66f61
Mark Latham <mark@votermedia.org>
2014-12-08 16:00:38 -0800

    Edit name; show gravatar.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
  
   def sign_up_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :email_monthly)
+    params.require(:user).permit(:email, :password, :password_confirmation, :email_monthly, :first_name, :last_name)
   end
  
   def account_update_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :email_monthly, :current_password)
+    params.require(:user).permit(:email, :password, :password_confirmation, :email_monthly, :first_name, :last_name, :current_password)
   end
 end
