9fc549f55d5d0f831f99c0705744439310c51881
Mark Latham <mark@votermedia.org>
2014-12-12 13:51:56 -0800

    Simplify password setup & change forms.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
- 
+
+  def change_password
+  end
+
   private
- 
+
   def sign_up_params
     params.require(:user).permit(:email, :password, :password_confirmation, :email_monthly, :first_name, :last_name)
   end
- 
+
   def account_update_params
     params.require(:user).permit(:email, :password, :password_confirmation, :email_monthly, :first_name, :last_name, :current_password)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Infocoop::Application.routes.draw do
-  root to: 'visitors#index'
-  devise_for :users, :path => '', :path_names => {:sign_up => 'register', :sign_in => 'login', :sign_out => 'logout'},
-             :controllers => { registrations: 'registrations' }
+  root 'visitors#index'
+  devise_for :users, path: '', path_names: {sign_up: 'register', sign_in: 'login', sign_out: 'logout'},
+             controllers: {registrations: 'registrations'}
+  devise_scope :user do
+    get "/change_password" => "registrations#change_password"
+  end
   resources :users
   resources :posts
-  get '/about',           :to => 'visitors#about',           :as => :about
-  get '/faq',             :to => 'visitors#faq',             :as => :faq
-  get '/privacy',         :to => 'visitors#privacy',         :as => :privacy
-  get '/terms',           :to => 'visitors#terms',           :as => :terms
-  get '/feed',            :to => 'posts#feed',               :as => :feed
+  get '/about',           to: 'visitors#about',                as: :about
+  get '/faq',             to: 'visitors#faq',                  as: :faq
+  get '/feed',            to: 'posts#feed',                    as: :feed
+  get '/privacy',         to: 'visitors#privacy',              as: :privacy
+  get '/terms',           to: 'visitors#terms',                as: :terms
 end
