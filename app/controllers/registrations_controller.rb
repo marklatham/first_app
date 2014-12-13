a354bbdd96530cd334f775ccaf538124839d03b8
Mark Latham <mark@votermedia.org>
2014-12-12 21:45:28 -0800

    Public posts feed only shows posts published by users with real_name.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -45,6 +45,6 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :email_monthly, :first_name, :last_name, :current_password)
+    params.require(:user).permit(:email, :password, :password_confirmation, :email_monthly, :first_name, :last_name, :current_password, :real_name)
   end
 end
