c18eb4a9cf016f856d5fbc1a8ffcd431aab58886
Mark Latham <mark@votermedia.org>
2014-12-18 16:26:14 -0800

    Display posts with html sanitized on "show" page; html tags removed in feeds.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,14 @@ module ApplicationHelper
       "#{page_title} | #{base_title}"
     end
   end
+
+  # Strips out html tags and replaces each with a space.
+  def space_tags(html_text)
+    if html_text.present?
+      (strip_tags(html_text.gsub '<', ' <').gsub '  ', ' ').gsub ' . ', '. '
+    else
+      html_text
+    end
+  end
+
 end
