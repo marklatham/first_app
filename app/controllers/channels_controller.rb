47752a3905c7904f8e8a4a0fed676884a1c16aa3
Mark Latham <mark@votermedia.org>
2015-01-06 14:51:02 -0800

    Can create and edit a channel.

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,2 +1,80 @@
 class ChannelsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @channel = Channel.new
+  end
+
+  def create
+    @channel = Channel.new(channel_params)
+    if @channel.save
+      flash[:notice] = "Channel created! Edit?"
+      redirect_to edit_channel_path(@channel)
+    else
+      flash[:alert] = "Sorry, couldn't create channel. Try again?"
+      render 'channels/new'
+    end
+  end
+
+  def edit
+    @channel = Channel.find(params[:id])
+  end
+
+  def update
+    @channel = Channel.find(params[:id])
+    if @channel.update_attributes(channel_params)
+      flash[:notice] = "Channel updated. Edit more?"
+    else
+      flash[:alert] = "Sorry, couldn't update channel. Try again?"
+    end
+    redirect_to edit_channel_path(@channel)
+  end
+
+  def update_display # This method is under construction!
+    @channel = Channel.find(params[:id])
+    if params[:commit] == 'Cancel unsaved changes'
+      flash[:notice] = "Unsaved changes cancelled."
+      redirect_to @channel and return
+    elsif @channel.update_attributes(display_params)
+      update_response(@channel, params); return if performed?
+    else
+      flash[:alert] = "Sorry, couldn't update channel. Try again?"
+    end
+    redirect_to @channel
+  end
+
+  def destroy
+    @channel.destroy
+    flash[:notice] = "Channel deleted"
+    redirect_to root_url
+  end
+
+  private
+
+    def channel_params
+      params.require(:channel).permit(:name, :manager_id, :display_id)
+    end
+
+    def display_params
+      params.require(:channel).permit(:display_id)
+    end
+
+    def update_response(channel, params) # This method is under construction!
+      if params[:commit] == 'Save & edit more'
+        flash[:notice] = "Channel saved."
+        redirect_to edit_channel_path(channel) and return
+      elsif params[:commit] == 'Convert this channel to html editing'
+        channel.custom = true
+        channel.save
+        flash[:notice] = "Channel converted."
+        redirect_to edit_channel_path(channel) and return
+      elsif params[:commit] == 'Dump custom html editing'
+        channel.custom = false
+        channel.save
+        flash[:alert] = "Custom html dumped."
+        redirect_to edit_channel_path(channel) and return
+      else
+        flash[:notice] = "Channel updated."
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Infocoop::Application.routes.draw do
+
   mount Bootsy::Engine => '/bootsy', as: 'bootsy'
   root 'visitors#index'
   devise_for :users, path: '', path_names: {sign_up: 'register', sign_in: 'login', sign_out: 'logout'},
@@ -6,12 +7,16 @@ Infocoop::Application.routes.draw do
   devise_scope :user do
     get "/change_password" => "registrations#change_password"
   end
+
   resources :users
   resources :posts
+  resources :channels
+
   get 'about',                to: 'visitors#about',            as: :about
   get 'editing_posts',        to: 'visitors#editing_posts',    as: :editing_posts
   get 'faq',                  to: 'visitors#faq',              as: :faq
   get 'feed',                 to: 'posts#feed',                as: :feed
   get 'privacy',              to: 'visitors#privacy',          as: :privacy
   get 'terms',                to: 'visitors#terms',            as: :terms
+
 end
