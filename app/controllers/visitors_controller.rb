30bbac7bb560f1f9cfc5b42493182cc08f7af471
Mark Latham <mark@votermedia.org>
2015-01-13 16:06:38 -0800

    First draft for home page "Info Mall" displays of all info channels.

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,7 @@
 class VisitorsController < ApplicationController
+
+  def index
+    @channels = Channel.where("display_id > 0")
+  end
+
 end
--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,6 +2,8 @@ class AdminMailer < ActionMailer::Base
 
   def new_registration(user)
     @user = user
-    mail from: "vaninfocoop@gmail.com", to: "vaninfocoop@gmail.com", subject: "New registration"
+    mail from: Rails.application.secrets.admin_email,
+           to: Rails.application.secrets.admin_email,
+      subject: "New registration"
   end
 end
