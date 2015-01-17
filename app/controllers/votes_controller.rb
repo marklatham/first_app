9bc5a29b748aa297277b144dd1e28c2a6d7e9876
Mark Latham <mark@votermedia.org>
2015-01-17 10:14:54 -0800

    Make code simpler and more readable; no substantive changes.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,19 +1,17 @@
 class VotesController < ApplicationController
 
   def vote_for_channel
-    @share = params[:share]
-    @channel = Channel.find(params[:channel_id])
-    @ip = request.remote_ip
-    @agent = request.user_agent
-    
+    share = params[:share]
+    channel = Channel.find(params[:channel_id])
+    ip = request.remote_ip
+    agent = request.user_agent
     if current_user
-      vote = Vote.create!({:share => @share, :channel_id => @channel.id, :ip => @ip, :agent => @agent, :user_id => current_user.id})
+      vote = Vote.create!({:share => share, :channel_id => channel.id, :ip => ip, :agent => agent, :user_id => current_user.id})
     else
-      vote = Vote.create!({:share => @share, :channel_id => @channel.id, :ip => @ip, :agent => @agent})
+      vote = Vote.create!({:share => share, :channel_id => channel.id, :ip => ip, :agent => agent})
     end
-    @ballot = find_ballot
-    @ballot.add_vote(vote)
-    
+    ballot = find_ballot
+    ballot.add_vote(vote)
     redirect_to :back
   end
 
